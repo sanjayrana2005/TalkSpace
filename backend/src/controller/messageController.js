a029d2457844c52c211c84a6d7197cf13340427f
Sanjay Rana <sanjayrana5113@gmail.com>
2026-02-03 23:15:59 +0530

    message route + controllers

--- backend/src/controller/messageController.js
+++ backend/src/controller/messageController.js
@@ -1,3 +1,32 @@
-export const sendMessage = () => {
-    
+const userModel = require("../models/userModel");
+
+const getAllContacts = async (req, res) => {
+    const loggedInUserId = req.user._id;
+    try {
+        const filteredUser = await userModel.find({ _id: { $ne: loggedInUserId } });
+        res.status(200).json({
+            filteredUser
+        })
+    } catch (error) {
+        res.json({
+            message: error.message
+        })
+    }
 }
+
+const getMessageByUserId = async (req, res) => {
+    try {
+        const myId = req.user._id;
+        const {id} = req.params;
+
+
+        const message =await mess
+    } catch (error) {
+        
+    }
+}
+
+module.exports = {
+    getAllContacts,
+    getMessageByUserId
+};
--- backend/src/router/messageRouter.js
+++ backend/src/router/messageRouter.js
@@ -1,6 +1,13 @@
 const express = require("express");
+const {getAllContacts,getMessageByUserId} = require("../controller/messageController");
+const protectRoute = require("../middleware/auth");
 
 const messageRouter = express.Router();
+
+messageRouter.get("/contacts",protectRoute,getAllContacts);
+// messageRouter.get("/chats",getChatPartners);
+messageRouter.get("/:id",getMessageByUserId);
+// messageRouter.post("/send/:id",sendMessage);
 
 
 module.exports=messageRouter;
