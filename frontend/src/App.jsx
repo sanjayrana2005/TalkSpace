704bdc32bce603ef2526fdbae5b2c9bd00a9b7d1
Sanjay Rana <sanjayrana5113@gmail.com>
2026-02-04 23:05:17 +0530

    daisyui setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,9 +2,10 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>
-      app
-    </div>
+    <button
+  class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900">
+  Button
+</button>
   )
 }
 
